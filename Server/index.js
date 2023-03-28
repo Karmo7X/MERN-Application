a7fef410158578752ed2184da5f08f20f30c46d3
mohamedo7x <mohamedo7x@hotmail.com>
2023-03-29 00:13:09 +0200

    API V2

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userModel from './model/Users.js'
-
+import cors from 'cors';
 
 
 /* Configuration */
 const app = express();
 
-
-
+/* Middle Ware */
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
+app.use(express.json());
 
 app.get('/users' ,async (req , res)=> {
     // res.status(200).json( await userModel.find());
@@ -16,9 +19,13 @@ app.get('/users' ,async (req , res)=> {
     res.status(200).json(getData);
 })
 
+app.post('/createUser' , async (req , res)=> {
+    const newUser = new userModel(req.body)
+    await newUser.save();
 
+    res.status(200).json(req.body)
+})
 
-/* Middle Ware */
 
 
 /* Connect DB */
